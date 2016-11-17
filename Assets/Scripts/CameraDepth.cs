da79e95e89569d6304f8e2750836c71b9e3a9e5b
Thomas Huang <vanlicht@gmail.com>
2016-11-17 00:26:34 -0800

    water shader v01.

---
+++ Assets/Scripts/CameraDepth.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraDepth : MonoBehaviour
+{
+    private Camera curCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!curCam)
+        {
+            curCam = GetComponent<Camera>();
+            return;
+        }
+        if (curCam.depthTextureMode == DepthTextureMode.None)
+        {
+            curCam.depthTextureMode = DepthTextureMode.Depth;
+        }
+    }
+}
